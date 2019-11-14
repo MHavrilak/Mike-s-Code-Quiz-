2d5e9343bc4727be0bb187814205ed72f1f11682
Hava-hizzle <mikehavrilak@gmail.com>
2019-11-14 18:36:55 -0500

    Quiz Revisions

--- assets/logic.js
+++ assets/logic.js
@@ -1,170 +1,65 @@
-// variables to keep track of quiz state
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-var timerId;
+var currScore = 0;
+var stbutt
+var time = 75;
+var timer = document.getElementById("time");
 
-// variables to reference DOM elements
-var questionsEl = document.getElementById("questions");
-var timerEl = document.getElementById("time");
-var choicesEl = document.getElementById("choices");
-var submitBtn = document.getElementById("submit");
-var startBtn = document.getElementById("start");
-var initialsEl = document.getElementById("initials");
-var feedbackEl = document.getElementById("feedback");
+var currQuest = 0;
 
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
-function startQuiz() {
-  // hide start screen
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "hide");
+function nextQuestion() {
+  var questTitl
+  questTitl = document.getElementById("questions-title");
+  questTitl.innerText = questions[currQuest].title
 
-  // un-hide questions section
-  questionsEl.removeAttribute("class");
+    var choicDiv
+    choicDiv = document.getElementById("choices");
+    
 
-  // start timer
-  timerId = setInterval(clockTick, 1000);
-
-  // show starting time
-  timerEl.textContent = time;
-
-  getQuestion();
-}
-
-function getQuestion() {
-  // get current question object from array
-  var currentQuestion = questions[currentQuestionIndex];
-
-  // update title with current question
-  var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
-
-  // clear out any old question choices
-  choicesEl.innerHTML = "";
-
-  // loop over choices
-  currentQuestion.choices.forEach(function(choice, i) {
-    // create new button for each choice
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
-
-    choiceNode.textContent = i + 1 + ". " + choice;
-
-    // attach click event listener to each choice
-    choiceNode.onclick = questionClick;
-
-    // display on the page
-    choicesEl.appendChild(choiceNode);
-  });
-}
-
-function questionClick() {
-  // check if user guessed wrong
-  if (this.value !== questions[currentQuestionIndex].answer) {
-    // penalize time
-    time -= 15;
-
-    if (time < 0) {
-      time = 0;
+  for(var i = 0; i < questions[currQuest].choices.length; i++) {
+    var choices = document.createElement("button");
+    choices.addEventListener("click", function() {
+    var answer = this.innerText
+    var currAnswer = questions[currQuest].answer;
+    var result
+    result = document.getElementById("result");
+    
+    if (answer == currAnswer) {
+      result.innerText = "correct";
+      currScore ++;
+    } else { 
+      result.innerText = "wrong";
+      time = time - 10;
     }
+    currQuest ++;
 
-    // display new time on page
-    timerEl.textContent = time;
+    choicDiv.innerHTML = "";
 
-    // play "wrong" sound effect
-    sfxWrong.play();
-
-    feedbackEl.textContent = "Wrong!";
-  } else {
-    // play "right" sound effect
-    sfxRight.play();
-
-    feedbackEl.textContent = "Correct!";
+    nextQuestion()
+    setTimeout(function() {
+      result.innerText = "";
+    }, 1500);
+    })
+    choices.innerText = questions[currQuest].choices[i]
+    choicDiv.appendChild(choices)
   }
+}
 
-  // flash right/wrong feedback on page for half a second
-  feedbackEl.setAttribute("class", "feedback");
-  setTimeout(function() {
-    feedbackEl.setAttribute("class", "feedback hide");
+stbutt = document.getElementById("start");
+stbutt.addEventListener("click", function(){
+  var trivQuest
+  trivQuest = document.getElementById("questions");
+  trivQuest.style.display = "block";
+  var stScreen
+  stScreen = document.getElementById("startscreen");
+  stScreen.style.display = "none";
+  console.log(trivQuest);
+  nextQuestion()
+  var interval = setInterval(function() {
+    time --;
+    timer.innerText = time;
+    if (time <= 0) {
+      clearInterval(interval);
+    }
+    
   }, 1000);
-
-  // move to next question
-  currentQuestionIndex++;
-
-  // check if we've run out of questions
-  if (currentQuestionIndex === questions.length) {
-    quizEnd();
-  } else {
-    getQuestion();
-  }
-}
-
-function quizEnd() {
-  // stop timer
-  clearInterval(timerId);
-
-  // show end screen
-  var endScreenEl = document.getElementById("end-screen");
-  endScreenEl.removeAttribute("class");
-
-  // show final score
-  var finalScoreEl = document.getElementById("final-score");
-  finalScoreEl.textContent = time;
-
-  // hide questions section
-  questionsEl.setAttribute("class", "hide");
-}
-
-function clockTick() {
-  // update time
-  time--;
-  timerEl.textContent = time;
-
-  // check if user ran out of time
-  if (time <= 0) {
-    quizEnd();
-  }
-}
-
-function saveHighscore() {
-  // get value of input box
-  var initials = initialsEl.value.trim();
-
-  // make sure value wasn't empty
-  if (initials !== "") {
-    // get saved scores from localstorage, or if not any, set to empty array
-    var highscores =
-      JSON.parse(window.localStorage.getItem("highscores")) || [];
-
-    // format new score object for current user
-    var newScore = {
-      score: time,
-      initials: initials
-    };
-
-    // save to localstorage
-    highscores.push(newScore);
-    window.localStorage.setItem("highscores", JSON.stringify(highscores));
-
-    // redirect to next page
-    window.location.href = "highscores.html";
-  }
-}
-
-function checkForEnter(event) {
-  // "13" represents the enter key
-  if (event.key === "Enter") {
-    saveHighscore();
-  }
-}
-
-// user clicks button to submit initials
-submitBtn.onclick = saveHighscore;
-
-// user clicks button to start quiz
-startBtn.onclick = startQuiz;
-
-initialsEl.onkeyup = checkForEnter;
+}) 
